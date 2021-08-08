34f292e21b1769ca71799648d1415b781aeead8c
phips4 <phips4.yt@gmail.com>
2021-08-08 19:46:00 +0200

    add install.sh and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # no-hibernate-when-ssh
+Prevent your linux (ubuntu) machine from hibernating if there are SSH connections.
+
+The following command adds logic to your .bashrc and .bash_logout files to disable hibernate mode when SSH connections are open and reenables it when all SSH sessions are closed.
+
+**disclaimer: currently work in progress**
+
+## installation
+
+```sh
+wget -O - https://raw.githubusercontent.com/phips4/no-hibernate-when-ssh/master/install.sh | sh
+```
+## todo list
+- [x] download and link script files to .bashrc and .bash_logout
+- [x] provide an installation 
+- [ ] disable hibernate mode on ssh activity
+- [ ] enable hibernate mode when all ssh sessions are closed
+- [ ] verify checksums on file download
+- [ ] provide a way to check if no hibernate is active
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+readonly ENABLE_FILE="enable_hibernate_mode.sh"
+readonly DISABLE_FILE="disable_hibernate_mode.sh"
+readonly ENABLE_FILE_REMOTE="https://raw.githubusercontent.com/phips4/no-hibernate-when-ssh/master/enable-hibernate.sh"
+readonly DISABLE_FILE_REMOTE="https://raw.githubusercontent.com/phips4/no-hibernate-when-ssh/master/disable-hibernate.sh"
+
+# command_name
+check_cmd () {
+  if ! command -v "$1" &> /dev/null; then
+      echo "ERROR: $1 needs to be installed."
+      exit
+  fi
+}
+
+# file_name, remote_file
+download () {
+  echo "downloading $2 to"
+  wget -q --show-progress -O "$HOME/$1" "$2"
+  chmod +x "$HOME/$1"
+  #TODO: verify checksum
+}
+
+# file_to_start, file
+link_file () {
+  START_SCRIPT="/bin/bash $1"
+  grep -qxF "$START_SCRIPT" "$2" || echo "$START_SCRIPT" >> "$2"
+  echo "linked $1 to $2"
+}
+
+check_cmd "wget"
+download $ENABLE_FILE $ENABLE_FILE_REMOTE
+download $DISABLE_FILE $DISABLE_FILE_REMOTE
+link_file $ENABLE_FILE "$HOME/.bashrc"
+link_file $DISABLE_FILE "$HOME/.bash_logout"
