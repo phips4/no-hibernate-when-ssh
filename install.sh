8d2f39a18d39a4acb043059c494b6bd7e795fbb6
phips4 <phips4.yt@gmail.com>
2021-08-20 21:14:05 +0200

    fix typos and small issues

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-readonly DIRECTORY='/etc/no-hibernate-when-ssh/'
+readonly DIRECTORY='/etc/no-hibernate-when-ssh'
 readonly SCRIPT_FILE='pam_ssh_no_hibernate.sh'
 readonly PAM_SSHD_FILE='/etc/pam.d/sshd'
 readonly ASK_FILE='ask.sh'
@@ -18,20 +18,22 @@ check_cmd () {
 # file_name, remote_file
 download () {
   echo "downloading $2 to"
-  wget -q --show-progress -O "$HOME/$1" "$2"
+  wget -q --show-progress -O "$DIRECTORY/$1" "$2"
   chmod +x "$HOME/$1"
   #TODO: verify checksum
 }
 
-check_cmd wget
-check_cmd system76-power
-check_cmd xset
+check_cmd 'wget'
+check_cmd 'system76-power'
+check_cmd 'xset'
 
-mkdir $DIRECTORY
+mkdir -p $DIRECTORY
 download $SCRIPT_FILE $SCRIPT_FILE_REMOTE
-download ASK_FILE $ASK_FILE_REMOTE
+download $ASK_FILE $ASK_FILE_REMOTE
 chmod a+x "$DIRECTORY/$SCRIPT_FILE"
 chmod a+x "$DIRECTORY/$ASK_FILE"
 
-echo '# no-hibernate-when-ssh' >> $PAM_SSHD_FILE
-echo "session     optional    pam_exec.so quiet $DIRECTORY/$SCRIPT_FILE" >> $PAM_SSHD_FILE
+if ! grep -L '# no-hibernate-when-ssh' $PAM_SSHD_FILE; then
+  printf '\n# no-hibernate-when-ssh\n' >> $PAM_SSHD_FILE
+  echo "session     optional    pam_exec.so quiet $DIRECTORY/$SCRIPT_FILE" >> $PAM_SSHD_FILE
+fi
